efdccd9bff3fa94cc6fc920773571ceba6adf53c
Guilherme Agostini <51136970+AgostiniGuilherme@users.noreply.github.com>
2025-05-05 14:37:46 -0300

    backend review

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,16 +1,15 @@
 from flask import Flask  
 from flask_sqlalchemy import SQLAlchemy  
 from flask_cors import CORS 
-import os
+
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
-
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
